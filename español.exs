46201a497c17a5c5a8e3f99e56d8b8aaeab30c87
Thibaut Barrère <thibaut.barrere@gmail.com>
2023-09-16 13:34:37 +0200

    Update español.exs

--- español.exs
+++ español.exs
@@ -1,4 +1,4 @@
-defmodule Numbers do
+defmodule Quizz do
   @numbers [
     {0, "cero"},
     {1, "uno"},
@@ -24,30 +24,87 @@ defmodule Numbers do
     {21, "veintiuno"},
     {22, "veintidós"},
     {23, "veintitrés"},
+    {24, "veinticuatro"},
+    {25, "veinticinco"},
     {26, "veintiséis"},
+    {27, "veintisiete"},
+    {28, "veintiocho"},
+    {29, "veintinueve"},
     {30, "treinta"}
   ]
 
+  @letters [
+    {"a", "a"},
+    {"b", "be"},
+    {"c", "ce"},
+    {"ch", "che"},
+    {"d", "de"},
+    {"e", "e"},
+    {"f", "efe"},
+    {"g", "ge"},
+    {"h", "hache"},
+    {"i", "i"},
+    {"j", "jota"},
+    {"k", "ka"},
+    {"l", "ele"},
+    {"ll", "ellé"},
+    {"m", "eme"},
+    {"n", "ene"},
+    {"ñ", "eñe"},
+    {"o", "o"},
+    {"p", "pe"},
+    {"q", "cu"},
+    {"r", "erre"},
+    {"s", "ese"},
+    {"t", "te"},
+    {"u", "u"},
+    {"v", "uve"},
+    {"w", "uve doble"},
+    {"x", "equis"},
+    {"y", "i griega"},
+    {"z", "zeta"}
+  ]
+
   def question({number, writing}) do
-    IO.puts "Comment écrit-on #{number |> inspect} en espagnol ?"
+    IO.puts("Comment écrit-on #{number |> inspect} en espagnol ?")
     {_, 0} = System.shell(~s(say #{number} --voice Marisol))
     data = IO.gets("")
     data = data |> String.trim()
+    data = String.replace(data, "o,", "ó")
+
     if data == writing do
-      IO.puts(IO.ANSI.green <> "OK" <> IO.ANSI.reset <> "\n")
+      IO.puts(IO.ANSI.green() <> "OK" <> IO.ANSI.reset() <> "\n")
     else
-      IO.puts IO.ANSI.format([:red, "KO!"])
-      IO.puts "La bonne réponse était: #{writing}\n"
+      IO.puts(IO.ANSI.format([:red, "KO!"]))
+      IO.puts("La bonne réponse était: #{writing}\n")
     end
   end
 
   def questions() do
-    IO.puts IO.ANSI.clear()
+    IO.puts(IO.ANSI.clear())
+
     @numbers
-    |> Enum.each(fn(number) -> 
+    |> Enum.each(fn number ->
       question(number)
+    end)
+  end
+
+  def question_letter({letter, name}) do
+    IO.puts("Comment prononce-t-on la lettre #{letter |> inspect} en espagnol ?")
+    IO.gets("")
+    IO.puts IO.ANSI.cyan <> name <> IO.ANSI.reset <> "\n"
+    {_, 0} = System.shell(~s(say #{name} --voice Marisol))
+  end
+
+  def quizz_alphabet() do
+    IO.puts(IO.ANSI.clear())
+
+    @letters
+    |> Enum.each(fn letter ->
+      question_letter(letter)
     end)
   end
 end
 
-Numbers.questions()
+# Numbers.questions()
+Quizz.quizz_alphabet()
